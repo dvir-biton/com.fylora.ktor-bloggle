e5e7ef1b15dbfa0bb62b9cf796772c3928f9e72d
fylora <fylora189@gmail.com>
2023-10-27 16:34:45 +0300

    :bug: send notification to offline users

--- src/main/kotlin/com/fylora/session/Bloggle.kt
+++ src/main/kotlin/com/fylora/session/Bloggle.kt
@@ -5,18 +5,31 @@ import com.fylora.session.model.ActiveUser
 import com.fylora.session.model.Comment
 import com.fylora.session.model.Post
 import com.fylora.session.model.Resource
+import com.fylora.session.notifications.Notification
+import com.fylora.session.notifications.NotifyUser
 import io.ktor.websocket.*
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import java.time.LocalDateTime
 
 class Bloggle {
 
     private val posts = mutableListOf<Post>()
     private val activeUsers = mutableListOf<ActiveUser>()
+    private val notifiedUsers = mutableListOf<NotifyUser>()
 
-    fun likePost(activeUser: ActiveUser, postId: String): Resource<String> {
+    suspend fun likePost(activeUser: ActiveUser, postId: String): Resource<String> {
         val post = posts.find { it.postId == postId }
             ?: return Resource.Error("Post not found")
+
+        notifyAuthor(
+            authorId = post.authorId,
+            notification = Notification.PostLiked(
+                by = activeUser.username,
+                timestamp = System.currentTimeMillis(),
+                postId = post.postId
+            )
+        )
 
         return if(post.userLiked.contains(activeUser.userId)) {
             post.userLiked.remove(activeUser.userId)
@@ -27,7 +40,7 @@ class Bloggle {
         }
     }
 
-    fun likeComment(activeUser: ActiveUser, commentId: String): Resource<String> {
+    suspend fun likeComment(activeUser: ActiveUser, commentId: String): Resource<String> {
         val post = posts.find { post ->
             post.comments.find { comment ->
                 comment.commentId == commentId
@@ -35,6 +48,15 @@ class Bloggle {
         } ?: return Resource.Error("Comment not found")
         val comment = post.comments.find { it.commentId == commentId }
             ?: return Resource.Error("Comment not found")
+
+        notifyAuthor(
+            authorId = post.authorId,
+            notification = Notification.CommentLiked(
+                by = activeUser.username,
+                timestamp = System.currentTimeMillis(),
+                postId = post.postId
+            )
+        )
 
         return if(comment.userLiked.contains(activeUser.userId)) {
             post.userLiked.remove(activeUser.userId)
@@ -64,6 +86,14 @@ class Bloggle {
                 )
             )
         }
+
+        val notifyUser = notifiedUsers.find { it.userId == user.id.toString() }
+        notifyUser?.notifications?.forEach {
+            activeUser.session.send(
+                Json.encodeToString(it)
+            )
+        }
+        notifiedUsers.remove(notifyUser)
 
         activeUsers.add(activeUser)
         return Resource.Success(activeUser)
@@ -102,7 +132,7 @@ class Bloggle {
         return Resource.Success("The user posted successfully")
     }
 
-    fun comment(activeUser: ActiveUser, postId: String, body: String): Resource<String> {
+    suspend fun comment(activeUser: ActiveUser, postId: String, body: String): Resource<String> {
         val comment = Comment(
             authorId = activeUser.userId,
             authorName = activeUser.username,
@@ -118,6 +148,37 @@ class Bloggle {
 
         post.comments.add(comment)
 
+        notifyAuthor(
+            authorId = post.authorId,
+            notification = Notification.Comment(
+                timestamp = System.currentTimeMillis(),
+                by = activeUser.username,
+                postId = postId
+            )
+        )
+
         return Resource.Success("The user posted successfully")
+    }
+
+    private suspend fun notifyAuthor(
+        authorId: String,
+        notification: Notification,
+    ) {
+        val author = activeUsers.find { it.userId == authorId }
+        if(author != null) {
+            author.session.send(Json.encodeToString(notification))
+        } else {
+            val notifiedUser = notifiedUsers.find { it.userId == authorId }
+            if(notifiedUser != null) {
+                notifiedUser.notifications.add(notification)
+            } else {
+                notifiedUsers.add(
+                    NotifyUser(
+                        notifications = mutableListOf(notification),
+                        userId = authorId
+                    )
+                )
+            }
+        }
     }
 }
